99e20989f026cee350ba00c54fd52e45a931913a
shvetsovdm <sorrows@mail.ru>
2015-02-20 13:27:57 +1000

    DEL unnecessary test

--- spec/views/main/show.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "main/show.html.erb", :type => :view do
-  it 'show login button' do
-    render
-    expect(rendered).to have_link 'Login with facebook'
-  end
-end
