7dbbe6d39d45fb3b075820b68154347fd2f124c0
shvetsovdm <sorrows@mail.ru>
2015-03-01 22:20:33 +1000

    CHANGE char-s pages now public

--- app/controllers/characteristics_controller.rb
+++ app/controllers/characteristics_controller.rb
@@ -1,6 +1,4 @@
 class CharacteristicsController < ApplicationController
-  before_action :authenticate
-
   def show
     @characteristic = Characteristic.find(params[:id])
     @page_title = @characteristic.title
