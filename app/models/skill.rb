904d7b15dcc444d1bb9931f3570daede9e4cdfb4
shvetsovdm <sorrows@mail.ru>
2015-03-01 14:11:12 +1000

    ADD counting lvl on skill list

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,6 @@
 class Skill < ActiveRecord::Base
   has_many :characteristics
   has_many :profiles, through: :characteristics
+  has_many :lvlups, through: :characteristics
   validates :title, presence: true, uniqueness: true
 end
