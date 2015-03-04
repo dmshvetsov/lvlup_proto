5ba404754e9c0cd5ef2f2ab33f3d36dfe4353aa5
shvetsovdm <sorrows@mail.ru>
2015-03-04 23:13:16 +1000

    ADD example profile link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def page_title
     @page_title || t(params['controller'] + params['action'])
   end
+
+  def shvetsovdm_profile
+    shvetsovdm = Profile.find_by(nickname: 'Dmitry Shvetsov')
+    profile_path shvetsovdm
+  end
 end
